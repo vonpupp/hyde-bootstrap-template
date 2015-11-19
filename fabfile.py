c03b425371ac1a4775acc3a008a745e6a4615700
vonpupp <vonpupp@gmail.com>
2015-11-18 23:36:58 -0200

    Tags showing properly

---
+++ fabfile.py
@@ -0,0 +1,37 @@
+from __future__ import with_statement
+from fabric.api import *
+
+HYDE_CONFIG = 'site.yaml'
+
+
+def _hyde(args):
+    return local('hyde -x {}'.format(args))
+
+
+def _clean():
+    local('rm -rf deploy')
+
+
+def _deploy():
+    _clean()
+    _hyde('gen')
+
+
+def _run():
+    _deploy()
+    local('cd deploy && python2 -m SimpleHTTPServer')
+
+
+@task
+def clean():
+    _clean()
+
+
+@task
+def deploy():
+    _deploy()
+
+
+@task
+def run():
+    _run()
